ce3aeb6f21c2f8f86094cb50468b8f5248414d3a
Rone <Gamewarrior1andbowser@gmail.com>
2025-11-13 08:47:55 -0600

    Built project bones

---
+++ assets/script/fakebook.js
@@ -0,0 +1,24 @@
+class User {
+
+  constructor(name, userName, email, id) {
+    this.name = name;
+    this.userName = userName;
+    this.email = email;
+    this.id = id;
+  }
+
+  getInfo() {
+
+  }
+}
+
+class Subscriber {
+
+  constructor(groups, pages, canMonetize) {
+    this.groups = groups;
+    this.pages = pages;
+    this.canMonetize = canMonetize;
+  }
+  
+  
+}
